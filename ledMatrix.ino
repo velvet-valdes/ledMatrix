e227c6ed240bc7520c3aab49267fb1fc151c5517
Velvet Valdes <velvet@valdes.com>
2017-07-29 21:26:53 -0400

    Serial Monitor Readout and Cleanup

--- ledMatrix.ino
+++ ledMatrix.ino
@@ -1,42 +1,9 @@
+#include <dht.h>          // We are using the DHT library found at http://playground.arduino.cc/Main/DHTLib
+#include <LedMatrix.h>    // Custom animation library found at https://github.com/velvet-valdes/ledMatrix_lib.git
+
 /*
-
-  LED matrix breadboard project
-  
-  Library functions available (so far):
-   
-    void turnOn();
-    void turnOff();
-    void turnOnAll();
-    void turnOffAll();
-    void downHorizontal(int count, int time);
-    void upHorizontal(int count, int time);
-    void downLeft(int count, int time);
-    void upRight(int count, int time);
-    void upLeft(int count, int time);
-    void downRight(int count, int time);
-    void snakeDown(int count, int time);
-    void snakeUp(int count, int time);
-    void randomOne(int count, int time);
-    void randomTwo(int count, int time);
-    void randomThree(int count, int time);
-    void blinkRed(int count, int time);
-    void blinkWhite(int count, int time);
-    void rowDown(int count, int time);
-    void rowUp(int count, int time);
-    void blinkSingleWhiteDown(int count, int time);
-    void blinkSingleRedDown(int count, int time);
-    void blinkSingleWhiteUp(int count, int time);
-    void blinkSingleRedUp(int count, int time);
-    void turnOnAllButOne(int count, int time);
-    void turnOnAllButTwo(int count, int time);
-        
-*/
-
-// We are using the DHT library found at http://playground.arduino.cc/Main/DHTLib
-#include <dht.h>
-
-// Include the custom LED matrix library
-#include <LedMatrix.h>
+ * Here we are taking the original LED matrix project and adding a temp / humidity sensor along with a digital light sensor
+ */
 
 dht DHT;  // I'm not sure what this line does but it does something like call or instantiate an object
 
@@ -44,20 +11,23 @@ dht DHT;  // I'm not sure what this line does but it does something like call or
 Matrix matrixMain(2,3,4,5,6,7,8,9);
 
 // Initialize global variables - note we added the buttons.
-int bankState = 16;	// initializing a value here to start the counter without resetting it throughout the loop 
+int bankState = 16;	      // initializing a value here to start the counter without resetting it throughout the loop 
 int timeHigh;
 int timeMedium;
 int timeLow;
 int timeSlow;
+int timeFloat;            // we're going to use this to store the value the digital light meter is reading 
 int cycleCount;
-int button01;   // our left button read pin on the arduino
-int button02;   // our right button read pin on the arduino
+int button01;             // our left button read pin on the arduino
+int button02;             // our right button read pin on the arduino
 int buttonState01;  
 int buttonState02; 
 int lightMeterIn = A0;    // this is our analog light intensity read pin (INPUT)
 int lightMeterState = 13; // this is our digital light state read pin which determines if the light is ON or OFF (INPUT)
 int valLightMeter;        // we are storing the value returned from the light meter here
 int valLightState;        // we are storing the value of the light state here.  0=LOW=ON 1=HIGH=OFF
+int valTemp;              // this is our temp reading in celsius
+int valHumidity;          // this is our relative humidity value in percentage
 
 // Set the pin we are going to read the data on for the temprature humidity sensor
 #define DHT11_PIN 12
@@ -86,11 +56,14 @@ void loop() {
 
   // Begin the temprature and humidty sensor check  
   int chk = DHT.read11(DHT11_PIN);
+
+  valTemp = ((DHT.temperature)*(1.8)+(32));
+  valHumidity = DHT.humidity;
   Serial.println(" ");
   Serial.print("Temperature = ");
-  Serial.println(DHT.temperature);
+  Serial.println(valTemp);
   Serial.print("Humidity = ");
-  Serial.println(DHT.humidity);
+  Serial.println(valHumidity);
 
   // Begin the light meter sensor check
   valLightMeter = analogRead(lightMeterIn);
@@ -101,37 +74,37 @@ void loop() {
   Serial.println(valLightMeter);
 
   // If the state of the light sensor is LOW (read as 0) it detects light.  If the sensor state is HIGH (read as 1) there is no light detected.
-  Serial.print("Light Sensor State =");
+  Serial.print("Light Sensor State = ");
   Serial.println(valLightState);
+  
+
+  // Print the bankState variable
+  Serial.print("Bank State = ");
+  Serial.println(bankState);
   Serial.println(" ");
 
 	// read the state of the pushbuttons
 	int buttonState01 = digitalRead(button01);
 	int buttonState02 = digitalRead(button02);
 
-
 	// check if the pushbutton is pressed.if it is, the buttonState is HIGH: When either button is pressed it will change the state of the counter variable.
   
 	if (buttonState01 == HIGH) {
 		// increment button state
 		bankState++;
-		Serial.println(bankState);
 		} 
 			
 	else {
 		// print value
-		Serial.println(bankState);
 	}
 
 	if (buttonState02 == HIGH) {
 		// decrement button state
 		bankState--;
-		Serial.println(bankState);
 		} 
 		
 	else {
 		// print value
-		Serial.println(bankState);
 	}
 
 	// We are moving to a fixed time value now that we have buttons
@@ -140,6 +113,7 @@ void loop() {
 	timeMedium = (100);
 	timeLow = (250);
 	timeSlow = (500);
+  timeFloat = valLightMeter;
 
 	// Changing the cycle count to a single count now that we have a button selecting states 
 	cycleCount = 1;  
@@ -148,71 +122,71 @@ switch (bankState) {
 
   case 0:
     // statements
-    matrixMain.downHorizontal(cycleCount, timeMedium);
+    matrixMain.downHorizontal(cycleCount, timeFloat);
     break;
   case 1:
     // statements
-    matrixMain.upHorizontal(cycleCount, timeMedium);
+    matrixMain.upHorizontal(cycleCount, timeFloat);
     break;
   case 2:
     // statements
-    matrixMain.downLeft(cycleCount, timeHigh);
+    matrixMain.downLeft(cycleCount, timeFloat);
     break;
   case 3:
     // statements
-    matrixMain.upRight(cycleCount, timeHigh);
+    matrixMain.upRight(cycleCount, timeFloat);
     break;
   case 4:
     // statements
-    matrixMain.upLeft(cycleCount, timeHigh);
+    matrixMain.upLeft(cycleCount, timeFloat);
     break;
   case 5:
     // statements
-    matrixMain.downRight(cycleCount, timeHigh);
+    matrixMain.downRight(cycleCount, timeFloat);
     break;
   case 6:
     // statements
-    matrixMain.snakeDown(cycleCount, timeLow);
+    matrixMain.snakeDown(cycleCount, timeFloat);
     break;
   case 7:
     // statements
-    matrixMain.snakeUp(cycleCount, timeLow);
+    matrixMain.snakeUp(cycleCount, timeFloat);
     break;
     case 8:
     // statements
-    matrixMain.blinkRed(cycleCount, timeMedium);
+    matrixMain.blinkRed(cycleCount, timeFloat);
     break;
     case 9:
     // statements
-    matrixMain.blinkWhite(cycleCount, timeMedium);
+    matrixMain.blinkWhite(cycleCount, timeFloat);
     break;
     case 10:
     // statements
-    matrixMain.rowDown(cycleCount, timeMedium);
+    matrixMain.rowDown(cycleCount, timeFloat);
     break;
     case 11:
     // statements
-    matrixMain.rowUp(cycleCount, timeMedium);
+    matrixMain.rowUp(cycleCount, timeFloat);
     break;
     case 12:
     // statements
-    matrixMain.blinkSingleWhiteDown(cycleCount, timeMedium);   
+    matrixMain.blinkSingleWhiteDown(cycleCount, timeFloat);   
     break; 
     case 13:
     // statements
-    matrixMain.blinkSingleRedDown(cycleCount, timeMedium);    
+    matrixMain.blinkSingleRedDown(cycleCount, timeFloat);    
     break;
     case 14:
     // statements
-    matrixMain.blinkSingleWhiteUp(cycleCount, timeMedium);
+    matrixMain.blinkSingleWhiteUp(cycleCount, timeFloat);
     break;
     case 15:
     // statements
-    matrixMain.blinkSingleRedUp(cycleCount, timeMedium);
+    matrixMain.blinkSingleRedUp(cycleCount, timeFloat);
     break;
     case 16:
     // statements
-    matrixMain.randomThree(cycleCount, timeLow);
+    matrixMain.randomThree(cycleCount, timeFloat);
     break;
     
 	}    
